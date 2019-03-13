9ff01d58456dbeeb984026a2211c8e041cc45ba5
Viroj Fernando <fernandoviroj@gmail.com>
2019-03-13 22:56:43 +0530

    optimised

--- src/app/services/tinder.message.retrival.service.ts
+++ src/app/services/tinder.message.retrival.service.ts
@@ -17,8 +17,11 @@ export class TinderAPI {
     profile_url;
 
     matchId;
-    t_token: string;
+
+    static t_token: string;
+
     recs_url: string;
+
     fb_auth_url: string;
 
     constructor(private httpClient: HttpClient) {
@@ -27,7 +30,7 @@ export class TinderAPI {
 
 
     services = {
-        init: (t_token: string) => {
+        init: () => {
             this.headers.append('Accept', 'application/json');
             this.headers.append('app-version', '1020333');
             this.headers.append('Content-Type', 'application/json');
@@ -36,7 +39,25 @@ export class TinderAPI {
             this.headers.append('Referer', 'https://tinder.com');
             this.headers.append('User-Agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
             this.headers.append('x-supported-image-formats', 'webp,jpeg');
-            this.headers.append('x-auth-token', this.t_token = t_token);
+            this.headers.append('x-auth-token', TinderAPI.t_token);
+
+
+            this.matches_url = "https://api.gotinder.com/v2/matches";
+            this.messages_url = "https://api.gotinder.com/v2/matches/" + this.matchId + "/messages";
+            this.profile_url = "https://api.gotinder.com/profile";
+            this.recs_url = "https://api.gotinder.com/user/recs";
+            this.fb_auth_url = "https://api.gotinder.com/auth";
+        },
+        initTinderToken: (t_token: string) => {
+            this.headers.append('Accept', 'application/json');
+            this.headers.append('app-version', '1020333');
+            this.headers.append('Content-Type', 'application/json');
+            this.headers.append('Origin', 'https://tinder.com');
+            this.headers.append('platform', 'web');
+            this.headers.append('Referer', 'https://tinder.com');
+            this.headers.append('User-Agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
+            this.headers.append('x-supported-image-formats', 'webp,jpeg');
+            this.headers.append('x-auth-token', TinderAPI.t_token = t_token);
 
 
             this.matches_url = "https://api.gotinder.com/v2/matches";
@@ -108,7 +129,7 @@ export class TinderAPI {
 
                 "message": "1"
 
-            } 
+            }
 
             return this.httpClient.get<any>(this.matches_url, { headers: new HttpHeaders(JSON.stringify(this.headers)), params: params })
 
@@ -116,19 +137,19 @@ export class TinderAPI {
 
         get_profile_of_the_logged_in_user: () => {
 
-            return this.httpClient.get<any>(this.profile_url, { headers: new HttpHeaders(JSON.stringify(this.headers))  })
+            return this.httpClient.get<any>(this.profile_url, { headers: new HttpHeaders(JSON.stringify(this.headers)) })
 
         },
         authenticate_with_fb: () => {
 
             return this.httpClient.get<any>(this.fb_auth_url, { headers: new HttpHeaders(JSON.stringify(this.headers)) })
 
-        }, 
+        },
 
         get_all_recomendations: () => {
 
             return this.httpClient.get<any>(this.recs_url, { headers: new HttpHeaders(JSON.stringify(this.headers)) })
-        } 
+        }
 
     };
 
