1432575897104cfbcdb809bc8234610411bde682
Viroj Fernando <fernandoviroj@gmail.com>
2019-03-19 15:26:20 +0530

    carousel

--- src/app/profile-card/profile-card.component.ts
+++ src/app/profile-card/profile-card.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
-import { NguCarouselConfig } from '@ngu/carousel';
+import { NguCarouselConfig, NguCarousel } from '@ngu/carousel';
 import { slider } from './animation';
 import { Observable, interval } from 'rxjs';
 
@@ -15,6 +15,7 @@ import { startWith, take, map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileCardComponent implements OnInit {
+ 
   @Input() profileDataSet = {
 
     name: '',
@@ -33,84 +34,85 @@ export class ProfileCardComponent implements OnInit {
 
 
   imgags = [
-    '../../assets/sample/sample.png', '../../assets/sample/sample2.png', '../../assets/sample/sample3.png'
+    '../../assets/sample/sample.png', '../../assets/sample/sample2.png'
   ];
 
   @Input() name: string;
 
   public carouselTileItems$: Array<any> = this.imgags;
   public carouselTileConfig: NguCarouselConfig = {
-    grid: { xs: 1, sm: 1, md: 1, lg: 5, all: 0 },
+    grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
     speed: 250,
     point: {
       visible: true
     },
     touch: true,
     loop: true,
-    interval: { timing: 5000 },
+    interval: { timing: 3000 },
     animation: 'lazy'
   };
   tempData: any[];
 
 
-  public carouselTiles = {
-    0: [],
-    1: [],
-    2: [],
-
-  };
 
   dropState = false;
 
   constructor(private sanitizer: DomSanitizer, private cdr: ChangeDetectorRef) { }
 
-  ngOnInit() {
-    // alert(JSON.parse(this.profileDataSet));
-
-
+  ngOnInit() { 
     this.img1 = this.sanitizer.bypassSecurityTrustStyle('url( ' + this.profileDataSet.imgUrl + ' )');
-
-
   }
-  carouselTileLoad(j: any): any {
-
-    const len = this.carouselTiles[j].length;
-
-    for (let i = 0; i < len; i++) {
-      this.carouselTiles[j].push(
-        this.imgags[i]
-      );
-    }
-  }
+ 
   allowDrop(ev) {
     ev.preventDefault();
 
   }
 
   drag(ev) {
+
     ev.dataTransfer.setData("text", ev.target.id);
+  
   }
 
   drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    alert(data)
-    // ev.target.appendChild(document.getElementById(data));
+
+    var data = ev.dataTransfer.getData("text"); 
+   
+    this.carouselTileItems$ = [];
+
+    for (var photos of JSON.parse( data).photos  ) {
+
+      this.carouselTileItems$.push(photos.processedFiles[0].url);
+     
+    } 
+
+    this.dropState = false; 
   }
-  draggedEnterData = '';
+ 
   dragenter(ev) {
+
     ev.preventDefault();
-    if (   ev.target.className === 'tile' ) {
+    
+    if (ev.target.className === 'tile') {
 
       this.dropState = true;
+    
     }
   }
 
   dragleave(ev) {
-    ev.preventDefault(); 
-    if (   ev.target.className === 'tile'  ) {
+    
+    ev.preventDefault();
+    
+    if (ev.target.className === 'tile') {
 
       this.dropState = false;
+    
     }
+  
   }
+
+
+ 
 }
